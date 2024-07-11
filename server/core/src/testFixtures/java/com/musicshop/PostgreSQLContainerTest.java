e50be951e7c4f4181f84000bdc472b0c9b7ba7ad
Morozov Dmitriy <jimmoroz@gmail.com>
2024-07-11 16:13:24 +0300

    fix: запуск всех тестов.
    В класс с контейнером postgres добавлена аннотация DirtiesContext.

--- server/core/src/testFixtures/java/com/musicshop/PostgreSQLContainerTest.java
+++ server/core/src/testFixtures/java/com/musicshop/PostgreSQLContainerTest.java
@@ -1,11 +1,13 @@
 package com.musicshop;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@DirtiesContext
 abstract class PostgreSQLContainerTest {
     @Container
     @ServiceConnection
